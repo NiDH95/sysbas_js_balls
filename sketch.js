958ce7cbc54786a8b69aef41704a3797d06335c5
NiDH95 <45459781+NiDH95@users.noreply.github.com>
2018-11-29 15:23:30 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,13 +2,12 @@ var x = 320;
 var y = 180;
 var x2 = 500;
 var y2 = 320;
-var xspeed = 14;
-var yspeed = 11;
-var xspeed2 = 12;
-var yspeed2 = 10;
+var xSpeed = 14;
+var ySpeed = 11;
+var xSpeed2 = 12;
+var ySpeed2 = 10;
 
-
-var r = 25;                                     //bepaalt de grootte van de bal
+var r = 25;
 var r2 = 60;
 
 function setup() {
@@ -21,24 +20,24 @@ function draw() {
   fill("purple");
   noStroke();
   ellipse(x, y, r*2, r*2);
-  x += xspeed;
-  y += yspeed;
+  x += xSpeed;
+  y += ySpeed;
   if (x > width - r || x < r) {
-    xspeed = -xspeed;
+    xSpeed = -xSpeed;
   }
   if (y > height - r || y < r) {
-    yspeed = -yspeed;
+    ySpeed = -ySpeed;
   }
 
   fill("red");
   noStroke();
   ellipse(x2, y2, r2*2, r2*2);
-  x2 += xspeed2;
-  y2 += yspeed2;
+  x2 += xSpeed2;
+  y2 += ySpeed2;
   if (x2 > width - r2 || x2 < r2) {
-    xspeed2 = -xspeed2;
+    xSpeed2 = -xSpeed2;
   }
   if (y2 > height - r2 || y2 < r2) {
-    yspeed2 = -yspeed2;
+    ySpeed2 = -ySpeed2;
   }
 }
